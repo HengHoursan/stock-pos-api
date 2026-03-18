73f4ec3d3a4550b9a35e112c967caf8247ca8602
HengHoursan <157660651+HengHoursan@users.noreply.github.com>
2026-03-18 16:51:57 +0700

    updated

--- src/main/java/com/example/stockpos/app/db/seeds/PermissionSeeder.java
+++ src/main/java/com/example/stockpos/app/db/seeds/PermissionSeeder.java
@@ -5,38 +5,37 @@ import com.example.stockpos.app.repository.PermissionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
 @RequiredArgsConstructor
 public class PermissionSeeder {
     private final PermissionRepository permissionRepository;
 
     public void seed() {
-        if (permissionRepository.count() == 0) {
-            seedEntityPermissions("User", "user");
-            seedEntityPermissions("Role", "role");
-            seedEntityPermissions("Permission", "permission");
-        }
+        seedEntityPermissions("User", "user");
+        seedEntityPermissions("Role", "role");
+        seedEntityPermissions("Permission", "permission");
+        seedEntityPermissions("Product", "product");
+        seedEntityPermissions("Category", "category");
     }
 
     private void seedEntityPermissions(String displayName, String name) {
         String group = displayName + " Management";
-        permissionRepository.saveAll(List.of(
-            buildPermission("all:" + name, "All " + displayName + "s", group, 1),
-            buildPermission("view:" + name, "View " + displayName + "s", group, 2),
-            buildPermission("create:" + name, "Create " + displayName, group, 3),
-            buildPermission("update:" + name, "Update " + displayName, group, 4),
-            buildPermission("delete:" + name, "Delete " + displayName, group, 5)
-        ));
+        
+        buildPermission("all:" + name, "All " + displayName + "s", group, 1);
+        buildPermission("view:" + name, "View " + displayName + "s", group, 2);
+        buildPermission("create:" + name, "Create " + displayName, group, 3);
+        buildPermission("update:" + name, "Update " + displayName, group, 4);
+        buildPermission("delete:" + name, "Delete " + displayName, group, 5);
     }
 
-    private Permission buildPermission(String name, String displayName, String group, Integer sort) {
-        return Permission.builder()
-                .name(name)
-                .displayName(displayName)
-                .group(group)
-                .sort(sort)
-                .build();
+    private void buildPermission (String name, String displayName, String group, Integer sort) {
+        if (!permissionRepository.existsByName(name)) {
+            permissionRepository.save(Permission.builder()
+                    .name(name)
+                    .displayName(displayName)
+                    .group(group)
+                    .sort(sort)
+                    .build());
+        }
     }
 }
